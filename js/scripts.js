bf6ba846a66166c3770ef54ae68e6d376c5af13c
David <daschrijn@gmail.com>
2020-10-22 00:29:00 -0700

    Update scripts.js
    add menu toggle method and smooth scroll.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
-var welcome = 'Hi! Welcome to my Portfolio Site';
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
 
-console.log(welcome);
+/* Smooth Scroll */
+
+$('a[href*="#"]').on('click', function(e) {
+  e.preventDefault()
+
+  $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top,
+    },
+    500,
+    'linear'
+  )
+})
