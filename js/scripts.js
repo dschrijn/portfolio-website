8afe96aa619e53fe61be7cd198aa367719642da5
David <daschrijn@gmail.com>
2020-11-03 15:40:49 -0800

    Update scripts.js
    fix code quality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
-    x.className += ' responsive';
+    x.className += ' responsive'
   } else {
-    x.className = 'navtoggle';
+    x.className = 'navtoggle'
   }
 }
 
 /* Smooth Scroll */
 
-$('a[href*="#"]').on('click', function(e) {
+$('a[href*="#"]').on('click', function (e) {
   e.preventDefault()
 
   $('html, body').animate({
-      scrollTop: $($(this).attr('href')).offset().top,
-    },
-    500,
-    'linear'
+    scrollTop: $($(this).attr('href')).offset().top
+  },
+  500,
+  'linear'
   )
 })
