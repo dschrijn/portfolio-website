2ac13e3c49611534eb4838b77c8459aec86ffe40
David <daschrijn@gmail.com>
2020-11-12 23:37:51 -0800

    Update scripts.js
    add "Scroll to top" function and SVG animation.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
-function menuToggle () {
+function menuToggle() {
   var x = document.getElementById('myNavtoggle')
   if (x.className === 'navtoggle') {
     x.className += ' responsive'
@@ -9,13 +9,37 @@ function menuToggle () {
 
 /* Smooth Scroll */
 
-$('a[href*="#"]').on('click', function (e) {
+$('a[href*="#"]').on('click', function(e) {
   e.preventDefault()
 
   $('html, body').animate({
-    scrollTop: $($(this).attr('href')).offset().top
-  },
-  500,
-  'linear'
+      scrollTop: $($(this).attr('href')).offset().top
+    },
+    500,
+    'linear'
   )
+})
+
+/* Scroll to top */
+
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {
+    $('#return-to-top').fadeIn(200)
+  } else {
+    $('#return-to-top').fadeOut(200)
+  }
+})
+$('#return-to-top').click(function() {
+  $('body,html').animate({
+    scrollTop: 0
+  }, 500)
+})
+
+/* Beam me up Animation */
+
+const beams = document.querySelectorAll('#beam');
+
+beams.forEach(beam => {
+  let duration = Math.random() * (1.2 - 0.6) + 0.6;
+  beam.style.animation = `beams ${duration}s infinite linear`;
 })
